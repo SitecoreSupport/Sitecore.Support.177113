c56639f497c2eee57c481781b5bfddd23fee3e7d
DK\msb <msb@sitecore.net>
2017-09-05 12:48:30 -0400

    Analytics definition database is set to the master database manulally

---
+++ src/Sitecore.Support.177113/Core/Pipelines/DispatchNewsletterPipeline/NotifyDispatchFinished.cs
@@ -0,0 +1,27 @@
+﻿using Sitecore.Analytics.Tracking;
+using Sitecore.ExM.Framework.Diagnostics;
+using Sitecore.Modules.EmailCampaign.Core;
+using Sitecore.Modules.EmailCampaign.Core.Pipelines.DispatchNewsletter;
+
+namespace Sitecore.Support.Modules.EmailCampaign.Core.Pipelines.DispatchNewsletter
+{
+    public class NotifyDispatchFinished : Sitecore.Modules.EmailCampaign.Core.Pipelines.DispatchNewsletter.NotifyDispatchFinished
+    {
+        // Methods
+        public NotifyDispatchFinished() : base()
+        {
+        }
+
+        public NotifyDispatchFinished(CoreFactory coreFactory, ILogger logger): base(coreFactory, logger)
+        {
+        }
+
+        public void Process(DispatchNewsletterArgs args)
+        {
+            using (new DefinitionDatabaseSwitcher("master"))
+            {
+                base.Process(args);
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.177113/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.177113")]
+[assembly: AssemblyProduct("Sitecore.Support.177113")]
+[assembly: ComVisible(false)]
